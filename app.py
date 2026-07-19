fb9f2cb6c285314a18c6a0d78e46eee7eb594c37
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-07-19 20:35:26 +0100

    feat(add up status, found at "/up")

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, app, render_template
+from flask import Flask, app, render_template, jsonify 
+import subprocess 
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
         return render_template('home.html')
+
+@app.route("/up")
+def up():
+        try:
+            commit = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], cwd="/root/braggingrights").decode().strip()
+            return jsonify({"commit": commit})
+        
+        except:
+            commit = "unknown"
+        
+        return jsonify({
+              "status": "online",
+              "service": "Bragging Rights, Owned by Jacob Navaratne",
+              "version" commit,
+              "last_updated": datetime.datetime.now(datetime.UTC).isoformat(),
+              "environment": "production"
+        })
 
 if __name__ == '__main__':
     app.run(debug=True, port=7834, host="0.0.0.0")
