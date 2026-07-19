4496da19fd6341a81fc45b6c98373be8b71ca77b
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-07-19 20:56:28 +0100

    fix(rss system)

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, app, render_template, jsonify 
+from flask import Flask, app, render_template, jsonify, Response
+import json
+from feedgen.feed import FeedGenerator
 import subprocess 
 import datetime
 
@@ -25,6 +27,46 @@ def up():
         "last_updated": datetime.datetime.now(datetime.UTC).isoformat(),
         "environment": "production"
     })
+
+@app.route("/updates.xml")
+def updates():
+    fg = FeedGenerator()
+
+    fg.id("https://braggingrights.mini-jacob.hackclub.app")
+    fg.title("Bragging Rights Deployments")
+    fg.link(
+        href="https://braggingrights.mini-jacob.hackclub.app/updates.xml"
+    )
+    fg.description("Automatic Bragging Rights deployment updates")
+
+    try:
+        with open("/root/braggingrights/deployments.json") as f:
+            deployments = json.load(f)
+
+        for deployment in deployments:
+            entry = fg.add_entry()
+
+            entry.id(deployment["commit"])
+            entry.title(
+                f"Deployment {deployment['commit']}"
+            )
+            entry.description(
+                deployment["message"]
+            )
+            entry.pubDate(
+                deployment["time"]
+            )
+
+    except Exception as e:
+        entry = fg.add_entry()
+        entry.id("error")
+        entry.title("RSS Error")
+        entry.description(str(e))
+
+    return Response(
+        fg.rss_str(),
+        mimetype="application/rss+xml"
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, port=7834, host="0.0.0.0")
